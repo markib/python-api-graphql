dd97a5d18e2700cdb1c09760025e9b6f993b4ad0
unknown <emarkib@gmail.com>
2024-06-25 15:40:15 -0500

    templates introduce

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,12 @@
-from flask import Flask
+import requests 
+import json
+from flask import Flask, render_template
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+import os
 
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder=os.path.join(os.pardir, "templates"))
 CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = (
@@ -11,6 +15,30 @@ app.config["SQLALCHEMY_DATABASE_URI"] = (
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+
 @app.route("/")
 def hello():
-    return "My First API !!"
+    url = "http://localhost:5000/graphql"
+
+    body = """ 
+        { 
+        listPosts {
+            success
+            errors
+            posts {
+            id
+            title 
+            description
+            created_at
+            }
+        }
+        } 
+        """
+    response = requests.post(url=url, json={"query": body})
+    json_data = response.json()
+
+    print("response status code: ", response.status_code)
+    if response.status_code == 200:
+        print("response : ", json_data["data"]["listPosts"]["posts"])
+    # return json_data["data"]["listPosts"]['posts']
+    return render_template("index.html",datas=json_data["data"]["listPosts"]["posts"])
--- api/queries.py
+++ api/queries.py
@@ -4,21 +4,21 @@ from ariadne import convert_kwargs_to_snake_case
 
 def listPosts_resolver(obj, info):
     try:
-        # posts = [post.to_dict() for post in Post.query.all()]
-        posts = [
-            {
-                "id": "1",
-                "title": "Post 1",
-                "description": "Content of Post 1",
-                "created_at": "2024-06-25T12:00:00Z",
-            },
-            {
-                "id": "2",
-                "title": "Post 2",
-                "description": "Content of Post 2",
-                "created_at": "2024-06-26T12:00:00Z",
-            },
-        ]
+        posts = [post.to_dict() for post in Post.query.all()]
+        # posts = [
+        #     {
+        #         "id": "1",
+        #         "title": "Post 1",
+        #         "description": "Content of Post 1",
+        #         "created_at": "2024-06-25T12:00:00Z",
+        #     },
+        #     {
+        #         "id": "2",
+        #         "title": "Post 2",
+        #         "description": "Content of Post 2",
+        #         "created_at": "2024-06-26T12:00:00Z",
+        #     },
+        # ]
         # posts = Post.findAll()
         print(posts)
         payload = {"success": True, "posts": posts}
